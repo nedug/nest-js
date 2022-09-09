c63f437f6afefe3c8dcb9b8aa7ef3b81eb129913
Sasha Temp <ru5nedug@mail.ru>
2022-09-09 15:57:53 +0300

    Table users

---
+++ src/users/users.model.ts
@@ -0,0 +1,24 @@
+import {BelongsToMany, Column, DataType, HasMany, Model, Table} from "sequelize-typescript";
+
+
+@Table({tableName: 'users'}) // Декторатор для того чтобы этот класс стал таблицей в базе данных с именем "users"
+export class User extends Model<User, > {
+    
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    // Декоратор, чтобы эти поля стали колонками в таблице. Указываем тип, уникальность, и тд.
+    id: number;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    banReason: string;
+
+
+}
