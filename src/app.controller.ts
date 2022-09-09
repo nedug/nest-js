7c144ae21e939eb3ac37211ac32ae2be5c452ede
Sasha Temp <ru5nedug@mail.ru>
2022-09-09 13:27:16 +0300

    Controller

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from "@nestjs/common";
+
+
+// Контроллеры отвечают за обработку входящих запросов и возврат ответов клиенту
+
+
+@('/api') // Указываем адрес запроса (можно тут не указывать)
+export class AppController {
+    
+    @Get('/users') // Указываем Декоратор метода HTTP
+    getUsers() {
+        return [{id: 1, name: 'Sasha'}]
+    }
+}
