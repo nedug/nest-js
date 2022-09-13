a5c4b81d5e404775c28cec3c16f378ee881eb1cb
Sasha Temp <ru5nedug@mail.ru>
2022-09-13 15:57:30 +0300

    class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
+import {ValidationPipe} from "./pipe/validation.pipe"
 
 
 async function start() {
@@ -20,6 +21,8 @@ async function start() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('/api/docs', app, document)
+
+    app.useGlobalPipes(new ValidationPipe())
 
     // Запускаем сервер
     await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
