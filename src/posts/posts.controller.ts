c55f900a44ad4b2261d206b66f1722b3c0f6805c
Sasha Temp <ru5nedug@mail.ru>
2022-09-13 16:00:50 +0300

    11

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,5 @@
 import { Controller } from '@nestjs/common';
 
+
 @Controller('posts')
 export class PostsController {}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 
+
 @Module({
   providers: [PostsService],
   controllers: [PostsController]
