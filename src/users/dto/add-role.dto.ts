1c5657ac42ccaa8a547e7f7b000fe2177ed65eaf
Sasha Temp <ru5nedug@mail.ru>
2022-09-13 14:54:38 +0300

    AddRoleDto

---
+++ src/users/dto/add-role.dto.ts
@@ -0,0 +1,13 @@
+// import {IsNumber, IsString} from "class-validator";
+
+// export class AddRoleDto {
+//     @IsString({message: "Должно быть строкой"})
+//     readonly value: string;
+//     @IsNumber({}, {message: "Должно быть числом"})
+//     readonly userId: number;
+// }
+
+export class AddRoleDto {
+    readonly value: string;
+    readonly userId: number;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,9 @@ import {CreateUserDto} from "./dto/create-user.dto";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {User} from "./users.model";
 import { RolesGuard } from 'src/auth/roles.guard';
+import {Roles} from "../auth/roles-auth.decorator";
+import {AddRoleDto} from "./dto/add-role.dto";
+import { BanUserDto } from './dto/ban-user.dto';
 
 
 @ApiTags('Пользователи')  // Для Swagger
@@ -18,14 +21,33 @@ export class UsersController {
     create(@Body() userDto: CreateUserDto) {
         return this.usersService.createUser(userDto);
     }
-
    
     @ApiOperation({summary: 'Получить всех пользователей'})
     @ApiResponse({status: 200, type: [User]})
+    @Roles("ADMIN")
     @UseGuards(RolesGuard)
     @Get()
     getAll() {
         return this.usersService.getAllUsers();
     }
+
+    @ApiOperation({summary: 'Выдать роль'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.usersService.addRole(dto);
+    }
+
+    @ApiOperation({summary: 'Забанить пользователя'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.usersService.ban(dto);
+    }
+
 
 }
