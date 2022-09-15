26f80f7030fb0a1240cb2582ce47772872d627b3
Alexander R <80103497+nedug@users.noreply.github.com>
2022-09-15 13:11:41 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
-import {ValidationPipe} from "./pipe/validation.pipe"
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from './pipe/validation.pipe';
 
 
 async function start() {
 
-    const PORT = process.env.PORT || 5001
+    const PORT = process.env.PORT || 5001;
 
     // Создаем приложение c нужным модулем
-    const app = await NestFactory.create(AppModule) 
+    const app = await NestFactory.create(AppModule);
 
     // Создаем документацию приложения и REST API
     const config = new DocumentBuilder()
@@ -17,16 +17,16 @@ async function start() {
         .setDescription('Документация REST API')
         .setVersion('1.0.0')
         .addTag('Sasha')
-        .build()
+        .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('/api/docs', app, document)
+    SwaggerModule.setup('/api/docs', app, document);
 
-    app.useGlobalPipes(new ValidationPipe())
+    app.useGlobalPipes(new ValidationPipe());
 
     // Запускаем сервер
-    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
 
-start()
+start();
